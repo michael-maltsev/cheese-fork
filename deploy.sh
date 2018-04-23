a0a9c9adbdd900c77bdd9473753490bbe2e367ac
michael-maltsev <36999014+michael-maltsev@users.noreply.github.com>
2018-04-24 01:55:31 +0300

    Fixed paths in deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ courses_file=courses_201702
 cd technion-ug-info-fetcher
 php courses_to_json.php || {
 	cd ..
-	echo 'courses_to_json failed'
+	echo courses_to_json failed
 	exit 1
 }
 cd ..
 
-src_file=technion-ug-info-fetcher\$courses_file.json
-dest_file=cheese-fork\$courses_file.js
+src_file=technion-ug-info-fetcher/$courses_file.json
+dest_file=deploy/$courses_file.js
 
 echo -n 'var courses_from_rishum = ' > $dest_file
 cat $src_file >> $dest_file
