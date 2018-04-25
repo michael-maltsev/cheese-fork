1996e6961bfc160951fde1d04b2da491fa960ee4
Michael Maltsev <36999014+michael-maltsev@users.noreply.github.com>
2018-04-25 10:50:48 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,5 +18,5 @@ echo -n 'var courses_from_rishum = ' > $dest_file_min
 cat $src_file >> $dest_file_min
 
 echo -n 'var courses_from_rishum = ' > $dest_file
-$php_cmd="echo json_encode(json_decode(file_get_contents('$src_file')), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);"
+php_cmd="echo json_encode(json_decode(file_get_contents('$src_file')), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);"
 php -r "$php_cmd" >> $dest_file
