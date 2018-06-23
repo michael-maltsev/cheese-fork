f62ceea51bd186005c9bb32b470934fd40a8675f
michael-maltsev <mmm.maltsev+personal@gmail.com>
2018-06-23 18:39:11 +0300

    Fetch all three available semesters from rishum

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-courses_file=courses_201702
+function fetch_semester {
+	local semester=$1
+	echo Fetching semester $semester...
 
-cd technion-ug-info-fetcher
-php courses_to_json.php || {
+	local courses_file=courses_$semester
+
+	cd technion-ug-info-fetcher
+	php courses_to_json.php courses_list_from_rishum=$semester || {
+		cd ..
+		echo courses_to_json failed
+		return 1
+	}
 	cd ..
-	echo courses_to_json failed
-	exit 1
+
+	local src_file=technion-ug-info-fetcher/$courses_file.json
+	local dest_file_min=deploy/$courses_file.min.js
+	local dest_file=deploy/$courses_file.js
+
+	echo -n 'var courses_from_rishum = ' > $dest_file_min
+	cat $src_file >> $dest_file_min
+
+	echo -n 'var courses_from_rishum = ' > $dest_file
+	local php_cmd="echo json_encode(json_decode(file_get_contents('$src_file')), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);"
+	php -r "$php_cmd" >> $dest_file
+
+	return 0
 }
-cd ..
 
-src_file=technion-ug-info-fetcher/$courses_file.json
-dest_file_min=deploy/$courses_file.min.js
-dest_file=deploy/$courses_file.js
-
-echo -n 'var courses_from_rishum = ' > $dest_file_min
-cat $src_file >> $dest_file_min
-
-echo -n 'var courses_from_rishum = ' > $dest_file
-php_cmd="echo json_encode(json_decode(file_get_contents('$src_file')), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);"
-php -r "$php_cmd" >> $dest_file
+fetch_semester 201701 || exit 1
+fetch_semester 201702 || exit 1
+fetch_semester 201703 || exit 1
+echo Done!
