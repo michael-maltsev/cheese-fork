e57ba50adf0020549cbdd55a3e69e78fad00731d
michael-maltsev <36999014+michael-maltsev@users.noreply.github.com>
2020-08-19 22:10:24 +0300

    Use weight icon for difficulty

--- deploy/components/course-feedback/course-feedback.js
+++ deploy/components/course-feedback/course-feedback.js
@@ -203,23 +203,45 @@ var CourseFeedback = (function () {
     }
 
     function makeRanksHtml(generalRank, difficultyRank, columnGrid) {
-        var makeStars = function (rank) {
+        var makeRanks = function (rank, full, half, empty) {
             var html = '';
             var rantTimesTwo = Math.round(rank * 2);
             for (var i = 0; i < 5; i++) {
                 if (i * 2 >= rantTimesTwo) {
-                    // Empty star.
-                    html += '<i class="far fa-star"></i>';
+                    html += empty;
                 } else if (i * 2 + 1 >= rantTimesTwo) {
-                    // Half star.
-                    html += '<i class="fas fa-star-half-alt"></i>';
+                    html += half;
                 } else {
-                    // Full star.
-                    html += '<i class="fas fa-star"></i>';
+                    html += full;
                 }
             }
 
             return html;
+        };
+
+        var makeStars = function (rank) {
+            var fullStar = '<i class="fas fa-star"></i>';
+            var halfStar = '<i class="fas fa-star-half-alt"></i>';
+            var emptyStar = '<i class="far fa-star"></i>';
+            return makeRanks(rank, fullStar, halfStar, emptyStar);
+        };
+
+        var makeWeights = function (rank) {
+            var full = '<i class="fas fa-weight-hanging"></i>';
+
+            // https://fontawesome.com/icons/weight-hanging?style=regular
+            var empty = '<i style="line-height: 0; display:inline-block;">' +
+                '<svg style="width: 1em;" aria-hidden="true" focusable="false" data-prefix="far" data-icon="weight-hanging" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-weight-hanging fa-1x">' +
+                '<path fill="currentColor" d="M510.28 445.86l-73.03-292.13c-3.8-15.19-16.44-25.72-30.87-25.72h-72.41c6.2-12.05 10.04-25.51 10.04-40 0-48.6-39.4-88-88-88s-88 39.4-88 88c0 14.49 3.83 27.95 10.04 40h-72.41c-14.43 0-27.08 10.54-30.87 25.72L1.72 445.86C-6.61 479.17 16.38 512 48.03 512h415.95c31.64 0 54.63-32.83 46.3-66.14zM216 88c0-22.06 17.94-40 40-40s40 17.94 40 40c0 22.05-17.94 40-40 40s-40-17.95-40-40zm246.72 376H49.28c-.7-.96-1.81-3.23-1-6.5L118.66 176h274.68l70.38 281.5c.81 3.27-.3 5.54-1 6.5z">' +
+                '</path></svg></i>';
+
+            var half = '<i class="fas fa-weight-hanging" style="width: calc(.5em + 3px); overflow: hidden;"></i>' +
+                '<i style="line-height: 0; display:inline-block; width: calc(.5em + 3px); overflow: hidden;">' +
+                '<svg style="width: 1em; left: calc(-.5em - 3px); position: relative;" aria-hidden="true" focusable="false" data-prefix="far" data-icon="weight-hanging" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-weight-hanging fa-1x">' +
+                '<path fill="currentColor" d="M510.28 445.86l-73.03-292.13c-3.8-15.19-16.44-25.72-30.87-25.72h-72.41c6.2-12.05 10.04-25.51 10.04-40 0-48.6-39.4-88-88-88s-88 39.4-88 88c0 14.49 3.83 27.95 10.04 40h-72.41c-14.43 0-27.08 10.54-30.87 25.72L1.72 445.86C-6.61 479.17 16.38 512 48.03 512h415.95c31.64 0 54.63-32.83 46.3-66.14zM216 88c0-22.06 17.94-40 40-40s40 17.94 40 40c0 22.05-17.94 40-40 40s-40-17.95-40-40zm246.72 376H49.28c-.7-.96-1.81-3.23-1-6.5L118.66 176h274.68l70.38 281.5c.81 3.27-.3 5.54-1 6.5z">' +
+                '</path></svg></i>';
+
+            return makeRanks(rank, full, half, empty);
         };
 
         columnGrid = columnGrid || 'lg';
@@ -237,7 +259,7 @@ var CourseFeedback = (function () {
                     '<div class="course-rank-title">עומס</div>' +
                     '<div class="course-rank-icons">' +
                         '<i class="fas fa-2x fa-feather-alt"></i>' +
-                        makeStars(difficultyRank) +
+                        makeWeights(difficultyRank) +
                         '<i class="fas fa-2x fa-dumbbell"></i>' +
                     '</div>' +
                 '</div>' +
