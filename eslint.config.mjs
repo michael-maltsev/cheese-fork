dc30123aa352f5a7b175bc02f5194fed43f38abf
michael-maltsev <36999014+michael-maltsev@users.noreply.github.com>
2025-12-18 14:05:14 +0200

    Update dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import compat from "eslint-plugin-compat";
+import globals from "globals";
+
+export default [
+    {
+        ignores: [
+            "modules/",
+            "node_modules/",
+            "dark-mode-toggle-stylesheets-loader.min.js",
+            "eslint.config.mjs",
+            "share-histograms.js",
+        ]
+    },
+    {
+        plugins: {
+            compat
+        },
+        languageOptions: {
+            ecmaVersion: 5,
+            sourceType: "script",
+            globals: {
+                ...globals.browser,
+                ...globals.jquery
+            }
+        },
+        rules: {
+            "compat/compat": "error",
+            "strict": ["error", "global"],
+            "indent": ["error", 4],
+            "no-unused-vars": ["error", { "args": "none", "caughtErrors": "none" }],
+            "semi": "error"
+        }
+    }
+];
